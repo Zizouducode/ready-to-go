9c20b60a926656aee71ae8557a45a7d759f78f57
Karim Hamga <karimhamga@macbook-pro-de-karim-1.home>
2022-11-02 16:30:21 +0100

    refactor switchs

--- src/components/Switch.js
+++ src/components/Switch.js
@@ -1,8 +1,28 @@
-// const Switch = (props) => {
-//   const handleSwitch1On = () => {
-//     setSwitch1(true);
-//   };
-//   return <div></div>;
-// };
+const Switch = (props) => {
+  const handleSwitchOn = () => {
+    props.setSwitch(true);
+  };
+  const handleSwitchOff = () => {
+    props.setSwitch(false);
+  };
+  return (
+    <div>
+      <div className="switch">
+        <div
+          className={props.switch === true ? "purple" : "white"}
+          onClick={handleSwitchOn}
+        >
+          ON
+        </div>
+        <div
+          className={props.switch === false ? "purple" : "white"}
+          onClick={handleSwitchOff}
+        >
+          OFF
+        </div>
+      </div>
+    </div>
+  );
+};
 
-// export default Switch;
+export default Switch;
--- src/components/Switchs.js
+++ src/components/Switchs.js
@@ -6,75 +6,17 @@ const Switchs = () => {
   const [switch2, setSwitch2] = useState(false);
   const [switch3, setSwitch3] = useState(false);
 
-  const handleSwitch1On = () => {
-    setSwitch1(true);
-  };
-  const handleSwitch1Off = () => {
-    setSwitch1(false);
-  };
-  const handleSwitch2On = () => {
-    setSwitch2(true);
-  };
-  const handleSwitch2Off = () => {
-    setSwitch2(false);
-  };
-  const handleSwitch3On = () => {
-    setSwitch3(true);
-  };
-  const handleSwitch3Off = () => {
-    setSwitch3(false);
-  };
-
   return (
     <div className="container">
       <div className="all-switch">
-        <div className="switch">
-          <div
-            className={switch1 === true ? "purple" : "white"}
-            onClick={handleSwitch1On}
-          >
-            ON
-          </div>
-          <div
-            className={switch1 === false ? "purple" : "white"}
-            onClick={handleSwitch1Off}
-          >
-            OFF
-          </div>
-        </div>
-        <div className="switch">
-          <div
-            className={switch2 === true ? "purple" : "white"}
-            onClick={handleSwitch2On}
-          >
-            ON
-          </div>
-          <div
-            className={switch2 === false ? "purple" : "white"}
-            onClick={handleSwitch2Off}
-          >
-            OFF
-          </div>
-        </div>
-        <div className="switch">
-          <div
-            className={switch3 === true ? "purple" : "white"}
-            onClick={handleSwitch3On}
-          >
-            ON
-          </div>
-          <div
-            className={switch3 === false ? "purple" : "white"}
-            onClick={handleSwitch3Off}
-          >
-            OFF
-          </div>
-        </div>
+        <Switch switch={switch1} setSwitch={setSwitch1} />
+        <Switch switch={switch2} setSwitch={setSwitch2} />
+        <Switch switch={switch3} setSwitch={setSwitch3} />
       </div>
       <div className={switch1 & switch2 & switch3 ? "green" : "red"}>
         {switch1 & switch2 & switch3 ? "GO ! " : "NO WAY !"}
       </div>
-      {/* <Switch switch1={switch1} setSwitch1={setSwitch1} /> */}
+
       <div
         className="reset"
         onClick={() => {
